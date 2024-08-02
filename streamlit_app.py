1fee25f3e0f67c0386b30c31b4de9035d0638098
ASHWINI24-May <avgarje@gmail.com>
2024-08-02 11:09:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,11 +36,11 @@ ingredients_string =''
 
 if ingredients_list:
     ingredients_string =''
-    
+
 for fruit_chosen in ingredients_list:
        ingredients_string += fruit_chosen + ''
 
-   
+set search on =fruit_chosen; 
 search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen,'SEARCH_ON'].iloc[0]
 st.write('The search value for', fruit_chosen,'is', search_on, '.')
     
